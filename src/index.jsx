0a5f292deeb6edde99eab9e66ea6c5db1b745409
Andrew Lubinger <amlubinger@gmail.com>
2023-10-04 01:17:49 -0400

    Fixed SIWA redirect urls.

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,39 @@ import './index.css';
 import AppContainer from './AppContainer';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import awsConfig from './aws-exports';
 
-Amplify.configure(config);
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+// Assuming you have two redirect URIs, and the first is for production and second is for localhost
+const [
+  productionRedirectSignIn,
+  localRedirectSignIn
+] = awsConfig.oauth.redirectSignIn.split(',');
+
+const [
+  productionRedirectSignOut,
+  localRedirectSignOut
+] = awsConfig.oauth.redirectSignOut.split(',');
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
+}
+
+Amplify.configure(updatedAwsConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
